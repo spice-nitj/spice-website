f7ca6afda14cc7efaebb8af9702d6269df38f1fb
Gautam Singla <ggsingla777@gmail.com>
2023-02-09 18:22:31 +0530

    Fix:build issues

--- components/Sponsors.tsx
+++ components/Sponsors.tsx
@@ -4,7 +4,7 @@ export default function Sponsors() {
       <div className='mx-auto max-w-7xl py-16 px-6 sm:py-24 lg:px-8'>
         <div className='lg:grid lg:grid-cols-2 lg:gap-8'>
           <h2 className='mx-auto max-w-md text-center text-3xl font-bold tracking-tight text-slate-900 lg:max-w-xl lg:text-left'>
-            The world's most innovative companies use our app
+            The world&apos;s most innovative companies use our app
           </h2>
           <div className='mt-8 flow-root self-center lg:mt-0'>
             <div className='-mt-4 -ml-8 flex flex-wrap justify-between lg:-ml-4'>
